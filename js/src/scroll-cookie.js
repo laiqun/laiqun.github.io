c3c702d2d0bffb41dec0a40096a488a15d816d73
lq <lq@lqdeMacBook-Air.local>
2017-11-26 12:05:44 +0800

    更新博客

---
+++ js/src/scroll-cookie.js
@@ -0,0 +1,3 @@
+// build time:Sun Nov 26 2017 12:05:10 GMT+0800 (CST)
+$(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
+//rebuild by neat 
